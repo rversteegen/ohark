f0105d576891552ee2b4171a7feb86ee9b2fd0fc
Ralph Versteegen <teeemcee@gmail.com>
2016-12-19 00:41:02 +1300

    Start on html rendering, and fix static file serving

--- src/website.py
+++ src/website.py
@@ -1,6 +1,7 @@
 import os.path
 from cgi import parse_qs, escape
 import sys
+import tabulate
 
 import gamedb
 
@@ -8,21 +9,51 @@ py2 = sys.version_info[0] == 2
 
 # mod_python_wsgi doesn't really set the path variables so that it's
 # possible to see where we are...
-WEB_ROOT = '/home/teeemcee/web'
+WEB_ROOT = './' #'/home/teeemcee/web'
+
+DATA_DIR = '../src/'
+
+print(os.path.abspath('.'))
+print(__file__)
 
 db = gamedb.GameList.load('cp')
 
-def text(obj):
+def encode(obj):
+    """Convert to correct format for returning to WSGI server"""
     if py2:
-        return escape(str(obj))
-    return escape(str(obj)).encode('utf-8')
+        return str(obj)
+    return str(obj).encode('utf-8')
 
-def application(environ, start_response):
+def text(obj):
+    # Apparently escape encodes as bytes?
+    return escape(str(obj)).replace('\n', '<br>\n')
+
+with open(DATA_DIR + 'page_template.html', 'r') as temp:
+    PAGE_TEMPLATE = temp.read()
+
+def render_page(content, title = 'OHR Archive'):
+    return [encode(PAGE_TEMPLATE.format(content = content, title = title))]
+
+def gamelist(games):
+    """
+    Render a list of games.
+    """
+    ret = tabulate.tabulate((game.columns() for game in db.games.values()), headers, 'html')
+    return render_page(ret)
+
+def static_serve(environ, start_response):
+    """Handles static file requests. Only needed when using wsgiref.simple_server"""
     fname = WEB_ROOT + environ['PATH_INFO']
     file_wrapper = environ['wsgi.file_wrapper']
 
     def send_file(fname):
-        start_response('200 OK', [('Content-Type', 'text/plain')])
+        ext = fname.split('.')[-1]
+        mimetype = {'txt': 'text/plain',
+                    'html': 'text/html',
+                    'css': 'text/css',
+                    'js': 'application/javascript',
+        }.get(ext, 'application/octet-stream')
+        start_response('200 OK', [('Content-Type', mimetype)])
         return file_wrapper(open(fname, 'rb'))
 
     if os.path.isfile(fname):
@@ -30,10 +61,21 @@ def application(environ, start_response):
     if os.path.isfile(fname + '/index.html'):
         return send_file(fname + '/index.html')
 
+def application(environ, start_response):
+    ret = static_serve(environ, start_response)
+    if ret:
+        return ret
+
     parameters = parse_qs(environ.get('QUERY_STRING', ''))
     param = ''
     if 'param' in parameters:
         param = escape(parameters['param'][0])
 
     start_response('200 OK', [('Content-Type', 'text/html')])
-    return [text(db.games), text(environ)]
+    headers = 'name', 'author', 'url', 'description'
+    ret = headers
+
+    #return [encode(ret)]
+##, db.games.keys()))#, headers)) #, 'html'))
+
+    return render_page(text(db.games) + text(environ))

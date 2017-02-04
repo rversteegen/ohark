530d0894f9a3cde8c5c65c8925b39758afb156a3
Ralph Versteegen <teeemcee@gmail.com>
2017-02-04 14:00:25 +1300

    Combine the functions for creating the ss and ss_links DBs to remove redundancy

--- src/pull_slimesalad.py
+++ src/pull_slimesalad.py
@@ -14,18 +14,26 @@ if py2:
 else:
     from io import StringIO
 
-
 def process_game_page(url):
-    """Returns description"""
     dom = scrape.get_page(url, 'windows-1252')
 
-    # Equivalent URLs for each game are
-    # viewgame.php?t= (topic number), viewgame.php?p= (post number)
-    # while viewtopic.php can be used instead of viewgame.php, but missing
-    # the tags. Use topic number as srcid. (See create_slimesalid_topic_lookup_table)
+    #Every game on SS has *four* valid links, for example:
+    # http://www.slimesalad.com/forum/viewgame.php?t=1021
+    # http://www.slimesalad.com/forum/viewtopic.php?t=1021
+    # http://www.slimesalad.com/forum/viewtopic.php?p=15109
+    # http://www.slimesalad.com/forum/viewgame.php?p=15109
+    # (?t= is topic number ?p= is post number)
+    # While viewtopic.php can be used instead of viewgame.php, it's missing
+    # the tags. Use topic number as srcid.
     assert 'viewgame.php?t=' in url and len(url.split('=')) == 2, "Expected game url to be viewgame.php?t=..."
     srcid = url.split('=')[1]
 
+    # Update the database mapping between the t= and p= links
+    link = dom.find(alt='Post').parent['href']
+    postnum = int(link.split('#')[-1])
+    topicnum = int(url.split('?t=')[-1])
+    link_db['p2t'][postnum] = topicnum
+    link_db['t2p'][topicnum] = postnum
 
     game = gamedb.Game()
     game.name = tostr(dom.find(class_='title').string)
@@ -83,6 +91,9 @@ def process_game_page(url):
     db.games[srcid] = game
 
 def process_index_page(url, limit = 9999):
+    """
+    Generate the db and link_db databases (both global variables)
+    """
     print("Fetching/parsing page...")
     page = scrape.get_url(url).decode('windows-1252')
 
@@ -93,40 +104,6 @@ def process_index_page(url, limit = 9999):
         limit -= 1
         if limit <= 0:
             break
-
-def create_slimesalid_topic_lookup_table(url, limit=999):
-    """
-    Every game on SS has *four* valid links, for example:
-    http://www.slimesalad.com/forum/viewgame.php?t=1021
-    http://www.slimesalad.com/forum/viewtopic.php?t=1021
-    http://www.slimesalad.com/forum/viewtopic.php?p=15109
-    http://www.slimesalad.com/forum/viewgame.php?p=15109
-    The preferred form is viewgame.php?t=... (the topic number is the srcid)
-    This creates the ss_links DB to map between topics and posts.
-    """
-    print("--Creating link lookup table--")
-    print("Fetching/parsing index...")
-    page = scrape.get_url(url).decode('windows-1252')
-
-    global link_db
-    link_db = {'p2t':{}, 't2p':{}}  # post -> topic and topic -> post mappings
-
-    file = StringIO(page)
-    for chunk in ChunkReader(file).each():
-        game = GameInfo(chunk)
-        print(game.url)
-        dom = scrape.get_page(game.url, 'windows-1252')
-        link = dom.find(alt='Post').parent['href']
-        postnum = int(link.split('#')[-1])
-        topicnum = int(game.url.split('?t=')[-1])
-        link_db['p2t'][postnum] = topicnum
-        link_db['t2p'][topicnum] = postnum
-
-        limit -= 1
-        if limit <= 0:
-            break
-
-    gamedb.DataBaseLayer.save('ss_links', link_db)
 
 link_db = gamedb.DataBaseLayer.cached_load('ss_links')
 
@@ -141,7 +118,6 @@ def srcid_for_SS_link(url):
         return None
 
     query = scrape.parse_qs(parsed.query)  # Parse to dict containing lists of values
-    print(query)
     if 't' in query:
         topicnum = int(query['t'][0])
         if topicnum not in link_db['t2p']:
@@ -153,12 +129,13 @@ def srcid_for_SS_link(url):
     return None
 
 if __name__ == '__main__':
-    # db = gamedb.GameList('ss')
-    # process_index_page('http://www.slimesalad.com/forum/gamedump.php')
-    # #process_game_page('http://www.slimesalad.com/forum/viewgame.php?t=5419')
-    # db.save()
+    db = gamedb.GameList('ss')
+    link_db = {'p2t':{}, 't2p':{}}  # post -> topic and topic -> post mappings
+    process_index_page('http://www.slimesalad.com/forum/gamedump.php')
+    #process_game_page('http://www.slimesalad.com/forum/viewgame.php?t=5419')
+    db.save()
+    gamedb.DataBaseLayer.save('ss_links', link_db)
 
-    create_slimesalid_topic_lookup_table('http://www.slimesalad.com/forum/gamedump.php')
     # Tests
     assert srcid_for_SS_link('http://www.slimesalad.com/forum/viewtopic.php?p=15109#15109') == 1021
     assert srcid_for_SS_link('http://www.slimesalad.com/forum/viewgame.php?t=345') == 345

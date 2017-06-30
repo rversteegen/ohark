d8c3a4a28cbbe7f6ca2131f7ecc0ac59f8ac1830
Ralph Versteegen <teeemcee@gmail.com>
2017-06-30 17:31:33 +1200

    pull_google_play.py: Pull games from a bunch of developer gamelists too

--- src/pull_google_play.py
+++ src/pull_google_play.py
@@ -13,7 +13,7 @@ from util import py2, tostr
 
 CACHE_INDEX = False
 
-def process_game_page(name, url):
+def process_game_page(url):
     """Returns description"""
     dom = scrape.get_page(url)
 
@@ -51,20 +51,55 @@ def process_game_page(name, url):
     db.games[srcid] = game
 
 def process_index_page(url):
+    "Returns a list of game URLs"
+    ret = []
     dom = scrape.get_page(url, cache = CACHE_INDEX)
 
     for link in dom.find_all('a', class_='title'):
-        name = link['title']
+        #name = link['title']
         gameurl = urljoin(url, link['href'])
         #print(name, gameurl)
-        process_game_page(name, gameurl)
+        ret.append(gameurl)
+    return ret
+
+def process_games(urls):
+    for gameurl in urls:
+        process_game_page(gameurl)
         #time.sleep(0.5)
 
+###############################################################################
 
 db = gamedb.GameList('googleplay')
 
-process_index_page('https://play.google.com/store/search?q=ohrrpgce&c=apps')
-#process_game_page("C. Kane", "https://play.google.com/store/apps/details?id=com.superwalrusland.ckane")
+# Collect games from various index pages
+gameurls = set()
+gameurls.update(process_index_page('https://play.google.com/store/search?q=ohrrpgce&c=apps'))
+gameurls.update(process_index_page('https://play.google.com/store/apps/developer?id=Hamster+Republic'))
+gameurls.update(process_index_page('https://play.google.com/store/apps/developer?id=Red+Triangle+Games'))
+gameurls.update(process_index_page('https://play.google.com/store/apps/developer?id=Spoonweaver+Studios'))
+gameurls.update(process_index_page('https://play.google.com/store/apps/developer?id=Super+Walrus+Games'))
+gameurls.update(process_index_page('https://play.google.com/store/apps/developer?id=A.+Hagen'))
+
+# Extra games not tagged with 'ohrrpgce' (from http://www.slimesalad.com/forum/viewtopic.php?p=127022#127022)
+# Missing games: Halalapszichiatrian, Macabre, Universal Wars Saga
+# see http://www.slimesalad.com/forum/viewtopic.php?p=127022#127022
+gameurls.add('https://play.google.com/store/apps/details?id=com.fyrewulff.swordofjade')
+# https://play.google.com/store/apps/details?id=com.superwalrusland.ghoststowns
+# https://play.google.com/store/apps/details?id=com.spoonweaver.slimeomancy
+# https://play.google.com/store/apps/details?id=com.redtrianglegames.surfasaurus
+# https://play.google.com/store/apps/details?id=com.hamsterrepublic.eatsoap
+# https://play.google.com/store/apps/details?id=com.hamsterrepublic.eatsoapzh
+# https://play.google.com/store/apps/details?id=com.hamsterrepublic.devwaffles.free
+# https://play.google.com/store/apps/details?id=com.hamsterrepublic.devwaffles
+# https://play.google.com/store/apps/details?id=com.hamsterrepublic.baconthulhu
+
+# Blacklist (non-OHR games):
+gameurls.discard('https://play.google.com/store/apps/details?id=com.hamsterrepublic.MantleMoonSea')
+gameurls.discard('https://play.google.com/store/apps/details?id=com.hamsterrepublic.vocabmosaic')
+gameurls.discard('https://play.google.com/store/apps/details?id=com.hamsterrepublic.stegavorto')
+
+# print(gameurls)
+process_games(gameurls)
 
 print(db.games)
 db.save()
